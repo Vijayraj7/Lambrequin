22609ed750daf9dec951c7b92c226928b89be9bd
Shajan P Shaji <shajanacumen@gmail.com>
2024-10-12 17:14:49 +0530

    design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,27 +5,50 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+        <title>Lambrequin</title>
     </head>
 
     <body>
 
-        <style>
-            #container{
-                height: 100px;
-                width: 100px;
-                background-color: red; 
-            }
-            #table{
-             width: 50%;
-             height: 50%;
-             background-color: blue;
-            }
-        </style>
-        <div id="container">
-            <div id="table">hii</div>
+        <div id="nav">
+            <div
+                style="background-color: green; width:100%; height: 100%;"></div>
+            <div id="click">
+                <a href>contact</a>
+                <a href="./abou">about</a>
+                <a href="./abou">about</a>
+                <a href="./abou">about</a>
+                <a href="./abou">about</a>
+                <img style="height: 30px;"
+                    src="https://png.pngtree.com/png-vector/20221212/ourmid/pngtree-red-arrow-sign-clipart-png-image_6520593.png"
+                    alt>
+            </div>
         </div>
 
     </body>
 
 </html>
+
+<style>
+    body{
+        padding: 0;
+        margin: 0;
+    }
+    #click{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        background-color: yellow;
+         width:100%;
+    }
+    #nav{
+        display: flex;
+        flex-direction: row;
+        justify-content: stretch;
+        align-items: space-around;
+        height: 100px;
+        width: 100%;
+        background-color: red; 
+    }
+    
+</style>
